0f93636d0e172b91e70dc2ead2114f1973ef08e8
Nataliapiesn <01123786@pw.edu.pl>
2020-11-22 13:48:33 +0000

    Modified at1

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -6,16 +6,19 @@ unsigned int transportujFlota(unsigned int towar)
 {
     unsigned int zlicz_zaglowce = 0;
     unsigned int przetransportowany = 0;
-    while (towar - przetransportowany > 0)
-    {
-      Stocznia stocznia{};
-      Statek* s1 = stocznia();
-      przetransportowany = przetransportowany + s1->transportuj();
-      if (s1 == dynamic_cast<Zaglowiec*>(s1))
+    if (towar == 0) {return 0;}
+    else{
+      while (przetransportowany < towar)
       {
-        zlicz_zaglowce++;
+        Stocznia stocznia{};
+        Statek* s1 = stocznia();
+        przetransportowany = przetransportowany + s1->transportuj();
+        if (s1 == dynamic_cast<Zaglowiec*>(s1))
+        {
+          zlicz_zaglowce++;
+        }
+        delete s1;
       }
-      delete s1;
     }
     return zlicz_zaglowce;
 }
