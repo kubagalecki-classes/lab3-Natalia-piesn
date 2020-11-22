98127454a6cd67300601ab100d317784091d4228
Nataliapiesn <01123786@pw.edu.pl>
2020-11-22 13:29:31 +0000

    Test- at. 1

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int zlicz_zaglowce = 0;
+    unsigned int przetransportowany = 0;
+    while (towar - przetransportowany > 0)
+    {
+      Stocznia stocznia{};
+      Statek* s1 = stocznia();
+      przetransportowany += s1->transportuj();
+      if (s1 == dynamic_cast< Zaglowiec* >( s1 ))
+      {
+        zlicz_zaglowce++;
+      }
+      delete s1;
+    }
+    return zlicz_zaglowce;
 }
